44bd3fa39d8cbe435ea8207a065076f4ba4368a3
danattali <danattali242@gmail.com>
2023-07-23 09:11:54 +0300

    new

--- script.js
+++ script.js
@@ -1,49 +1,98 @@
+// Task class creation
 var Task = /** @class */ (function () {
-    function Task(description) {
+    function Task(description, completed, id) {
+        if (completed === void 0) { completed = false; }
+        if (id === void 0) { id = Math.floor(Math.random() * 1001); }
         this.description = description;
-        this.id = Math.floor(Math.random() * 1001);
+        this.id = id;
         this.description = description;
-        this.completed = false;
+        this.completed = completed;
     }
     return Task;
 }());
-var task1 = new Task("HW");
-console.log(task1);
+// let task1 = new Task("HW");
+// console.log(task1);
+// TaskManager creation
 var TaskManager = /** @class */ (function () {
     function TaskManager() {
-        this.tasks = [];
+        this.tasks = localStorage.getItem("Tasks")
+            ? JSON.parse(localStorage.getItem("Tasks"))
+            : [];
     }
-    TaskManager.prototype.addTask = function (description) {
-        this.tasks.push(new Task(description));
+    TaskManager.prototype.addTask = function (description, completed) {
+        this.tasks.push(new Task(description, completed));
+        localStorage.setItem("Tasks", JSON.stringify(manager.tasks));
     };
     TaskManager.prototype.deleteTask = function (id) {
         var indexToDelete = this.tasks.findIndex(function (task) { return task.id == id; });
         this.tasks.splice(indexToDelete, 1);
+        localStorage.setItem("Tasks", JSON.stringify(manager.tasks));
     };
-    TaskManager.prototype.updateTypeDescription = function (id, newDescription) {
+    TaskManager.prototype.updateTaskDescription = function (id, newDescription) {
         var indexToUpdate = this.tasks.findIndex(function (task) { return task.id == id; });
         this.tasks[indexToUpdate].description = newDescription;
+        localStorage.setItem("Tasks", JSON.stringify(manager.tasks));
     };
     TaskManager.prototype.completeTask = function (id) {
         var indexToUpdate = this.tasks.findIndex(function (task) { return task.id == id; });
         this.tasks[indexToUpdate].completed = true;
+        localStorage.setItem("Tasks", JSON.stringify(manager.tasks));
+    };
+    TaskManager.prototype.activateTask = function (id) {
+        var indexToUpdate = this.tasks.findIndex(function (task) { return task.id == id; });
+        this.tasks[indexToUpdate].completed = false;
+        localStorage.setItem("Tasks", JSON.stringify(manager.tasks));
     };
     return TaskManager;
 }());
 var manager = new TaskManager();
-manager.addTask("Dishes");
-manager.addTask("Home work");
-console.log(manager.tasks);
-/* function showTasksInTable(): void {
-    for (let task of manager.tasks) {
-        document.getElementById("tasks")!.innerHTML += `<tr> <td> ${task.id} </td> <td> ${task.description} </td> <td> ${task.completed} </td> </tr>`;
+console.log(manager);
+// showTasksInLists();
+// Original (B4 LocalStorage)
+function showTasksInLists() {
+    document.getElementById("active").innerHTML = "";
+    document.getElementById("complete").innerHTML = "";
+    for (var _i = 0, _a = manager.tasks; _i < _a.length; _i++) {
+        var task = _a[_i];
+        if (task.completed == false) {
+            document.getElementById("active").innerHTML += "\n     <div> <li class=\"list-group-item d-inline-block w-50\">".concat(task.description, "</li> <span> <button class=\"btn btn-success\" onclick=\"completeTask(").concat(task.id, ")\"><i class=\"fa-solid fa-check\"></i></button> <button class=\"btn btn-primary\" onclick=\"updateDescription(").concat(task.id, ")\"><i class=\"fa-solid fa-pen\"></i></button> <button class=\"btn btn-danger\" onclick=\"deleteTask(").concat(task.id, ")\"><i class=\"fa-solid fa-trash\"></i></button></span> </div> ");
+        }
+        else {
+            document.getElementById("complete").innerHTML += "\n      <div> <li class=\"list-group-item d-inline-block w-50 text-decoration-line-through\">".concat(task.description, "</li> <span> <button class=\"btn btn-success\" onclick=\"reassignTask(").concat(task.id, ")\"><i class=\"fa-solid fa-check-double\"></i></button> <button class=\"btn btn-primary\" disabled><i class=\"fa-solid fa-pen\"></i></button> <button class=\"btn btn-danger\" onclick=\"deleteTask(").concat(task.id, ")\"><i class=\"fa-solid fa-trash\"></i></button></span> </div> ");
+        }
     }
 }
-showTasksInTable(); */
+showTasksInLists();
 function completeTask(id) {
     manager.completeTask(id);
     showTasksInLists();
 }
+showTasksInLists();
+var addNewTask = function () {
+    var description = document.getElementById("newTask")
+        .value;
+    if (description != "" || description != null) {
+        var completed = document.getElementById("completed").checked == true
+            ? true
+            : false;
+        console.log(document.getElementById("completed").value);
+        document.getElementById("newTask").value = "";
+        document.getElementById("completed").checked = false;
+        manager.addTask(description, completed);
+        showTasksInLists();
+    }
+    else {
+        alert("Please add Task description");
+    }
+};
+var reassignTask = function (id) {
+    manager.activateTask(id);
+    showTasksInLists();
+};
+var completeActiveTask = function (id) {
+    manager.completeTask(id);
+    showTasksInLists();
+};
 function deleteTask(id) {
     // confirm "Are you sure?"
     if (confirm("Are you sure?")) {
@@ -55,23 +104,20 @@ function updateDescription(id) {
     // prompt for new description
     var newDescription = prompt("Enter new description:");
     if (newDescription != null || newDescription != "") {
-        manager.updateTypeDescription(id, newDescription);
+        manager.updateTaskDescription(id, newDescription);
         showTasksInLists();
     }
     else
         alert("Sorry! Something went wrong");
 }
-function showTasksInLists() {
-    document.getElementById("active").innerHTML = "";
-    document.getElementById("completed").innerHTML = "";
-    for (var _i = 0, _a = manager.tasks; _i < _a.length; _i++) {
-        var task = _a[_i];
-        if (task.completed == false) {
-            document.getElementById("active").innerHTML += "<div> <li class=\"list-group-item d-inline-block w-50\">".concat(task.description, "</li> <span> <button class=\"btn btn-success\" onclick=\"completeTask(").concat(task.id, ")\"><i class=\"fa-solid fa-check\"></i></button> <button class=\"btn btn-primary\" onclick=\"updateDescription(").concat(task.id, ")\"><i class=\"fa-solid fa-pen\"></i></button> <button class=\"btn btn-danger\" onclick=\"deleteTask(").concat(task.id, ")\"><i class=\"fa-solid fa-trash\"></i></button></span> </div> ");
-        }
-        else {
-            document.getElementById("completed").innerHTML += "<div> <li class=\"list-group-item d-inline-block w-50 text-decoration-line-through\">".concat(task.description, "</li> <span> <button class=\"btn btn-success\" disabled><i class=\"fa-solid fa-check-double\"></i></button> <button class=\"btn btn-primary\" disabled><i class=\"fa-solid fa-pen\"></i></button><button class=\"btn btn-danger\" disabled><i class=\"fa-solid fa-trash\"></i></button></span></div>");
-        }
-    }
+console.log(manager.tasks);
+//Copyrights
+function copyrights() {
+    var developer = "Oved Harari";
+    var date = new Date();
+    var currentYear = date.getFullYear();
+    console.log(currentYear);
+    document.getElementById("copyrightYear").innerText = " " + currentYear;
+    document.getElementById("developer").innerText = developer;
 }
-showTasksInLists();
+copyrights();
--- script.ts
+++ script.ts
@@ -1,83 +1,147 @@
+// Task class creation
 class Task {
-    public id: number;
-    public completed: boolean
+  public id: number;
+  public completed: boolean;
 
-    constructor(public description: string,) {
-        this.id = Math.floor(Math.random() * 1001);
-        this.description = description;
-        this.completed = false;
-
-    }
+  constructor(
+    public description: string,
+    completed: boolean = false,
+    id: number = Math.floor(Math.random() * 1001)
+  ) {
+    this.id = id;
+    this.description = description;
+    this.completed = completed;
+  }
 }
 
-let task1 = new Task("HW");
-console.log(task1);
+// let task1 = new Task("HW");
+// console.log(task1);
+
+// TaskManager creation
 
 class TaskManager {
-    public tasks: Task[];
-    constructor() {
-        this.tasks = [];
-    }
-    addTask(description: string): void {
-        this.tasks.push(new Task(description));
-    }
-    deleteTask(id: number): void {
-        let indexToDelete = this.tasks.findIndex((task: Task) => task.id == id);
-        this.tasks.splice(indexToDelete, 1);
-    }
-    updateTypeDescription(id: number, newDescription: string): void {
-        let indexToUpdate = this.tasks.findIndex((task: Task) => task.id == id)
-        this.tasks[indexToUpdate].description = newDescription;
-    }
-    completeTask(id: number): void {
-        let indexToUpdate = this.tasks.findIndex((task: Task) => task.id == id);
-        this.tasks[indexToUpdate].completed = true;
-    }
+  public tasks: Task[];
+  constructor() {
+    this.tasks = localStorage.getItem("Tasks")
+      ? JSON.parse(localStorage.getItem("Tasks")!)
+      : [];
+  }
+  addTask(description: string, completed: boolean): void {
+    this.tasks.push(new Task(description, completed));
+    localStorage.setItem("Tasks", JSON.stringify(manager.tasks));
+  }
+  deleteTask(id: number): void {
+    let indexToDelete = this.tasks.findIndex((task: Task) => task.id == id);
+    this.tasks.splice(indexToDelete, 1);
+    localStorage.setItem("Tasks", JSON.stringify(manager.tasks));
+  }
+  updateTaskDescription(id: number, newDescription: string): void {
+    let indexToUpdate = this.tasks.findIndex((task: Task) => task.id == id);
+    this.tasks[indexToUpdate].description = newDescription;
+
+    localStorage.setItem("Tasks", JSON.stringify(manager.tasks));
+  }
+  completeTask(id: number): void {
+    let indexToUpdate = this.tasks.findIndex((task: Task) => task.id == id);
+    this.tasks[indexToUpdate].completed = true;
+    localStorage.setItem("Tasks", JSON.stringify(manager.tasks));
+  }
+  activateTask(id: number): void {
+    let indexToUpdate = this.tasks.findIndex((task: Task) => task.id == id);
+    this.tasks[indexToUpdate].completed = false;
+    localStorage.setItem("Tasks", JSON.stringify(manager.tasks));
+  }
 }
+
 let manager = new TaskManager();
-manager.addTask("Dishes");
-manager.addTask("Home work");
-console.log(manager.tasks);
 
+console.log(manager);
 
-/* function showTasksInTable(): void {
-    for (let task of manager.tasks) {
-        document.getElementById("tasks")!.innerHTML += `<tr> <td> ${task.id} </td> <td> ${task.description} </td> <td> ${task.completed} </td> </tr>`;
+// showTasksInLists();
+
+// Original (B4 LocalStorage)
+function showTasksInLists() {
+  document.getElementById("active")!.innerHTML = "";
+  document.getElementById("complete")!.innerHTML = "";
+  for (let task of manager.tasks) {
+    if (task.completed == false) {
+      document.getElementById("active")!.innerHTML += `
+     <div> <li class="list-group-item d-inline-block w-50">${task.description}</li> <span> <button class="btn btn-success" onclick="completeTask(${task.id})"><i class="fa-solid fa-check"></i></button> <button class="btn btn-primary" onclick="updateDescription(${task.id})"><i class="fa-solid fa-pen"></i></button> <button class="btn btn-danger" onclick="deleteTask(${task.id})"><i class="fa-solid fa-trash"></i></button></span> </div> `;
+    } else {
+      document.getElementById("complete")!.innerHTML += `
+      <div> <li class="list-group-item d-inline-block w-50 text-decoration-line-through">${task.description}</li> <span> <button class="btn btn-success" onclick="reassignTask(${task.id})"><i class="fa-solid fa-check-double"></i></button> <button class="btn btn-primary" disabled><i class="fa-solid fa-pen"></i></button> <button class="btn btn-danger" onclick="deleteTask(${task.id})"><i class="fa-solid fa-trash"></i></button></span> </div> `;
     }
+  }
 }
-showTasksInTable(); */
 
-
+showTasksInLists();
 
 function completeTask(id: number) {
-    manager.completeTask(id);
-    showTasksInLists();
+  manager.completeTask(id);
+  showTasksInLists();
 }
-function deleteTask(id: number) {
-    // confirm "Are you sure?"
-    if (confirm("Are you sure?")) {
-        manager.deleteTask(id);
-        showTasksInLists();
-    }
 
-}
-function updateDescription(id: number) {
-    // prompt for new description
-    let newDescription = prompt("Enter new description:");
-    if (newDescription != null || newDescription != "") {
-        manager.updateTypeDescription(id, newDescription!);
-        showTasksInLists();
-    } else alert("Sorry! Something went wrong");
-}
-function showTasksInLists() {
-    document.getElementById("active")!.innerHTML = "";
-    document.getElementById("completed")!.innerHTML = "";
-    for (let task of manager.tasks) {
-        if (task.completed == false) {
-            document.getElementById("active")!.innerHTML += `<div> <li class="list-group-item d-inline-block w-50">${task.description}</li> <span> <button class="btn btn-success" onclick="completeTask(${task.id})"><i class="fa-solid fa-check"></i></button> <button class="btn btn-primary" onclick="updateDescription(${task.id})"><i class="fa-solid fa-pen"></i></button> <button class="btn btn-danger" onclick="deleteTask(${task.id})"><i class="fa-solid fa-trash"></i></button></span> </div> `;
-        } else {
-            document.getElementById("completed")!.innerHTML += `<div> <li class="list-group-item d-inline-block w-50 text-decoration-line-through">${task.description}</li> <span> <button class="btn btn-success" disabled><i class="fa-solid fa-check-double"></i></button> <button class="btn btn-primary" disabled><i class="fa-solid fa-pen"></i></button><button class="btn btn-danger" disabled><i class="fa-solid fa-trash"></i></button></span></div>`;
-        }
-    }
-}
 showTasksInLists();
+
+let addNewTask = () => {
+  let description = (document.getElementById("newTask") as HTMLInputElement)
+    .value;
+  if (description != "" || description != null) {
+    let completed =
+      (document.getElementById("completed") as HTMLInputElement).checked == true
+        ? true
+        : false;
+    console.log(
+      (document.getElementById("completed") as HTMLInputElement).value
+    );
+
+    (document.getElementById("newTask") as HTMLInputElement).value = "";
+    (document.getElementById("completed") as HTMLInputElement).checked = false;
+
+    manager.addTask(description, completed);
+
+    showTasksInLists();
+  } else {
+    alert("Please add Task description");
+  }
+};
+
+let reassignTask = (id: number) => {
+  manager.activateTask(id);
+  showTasksInLists();
+};
+
+let completeActiveTask = (id: number) => {
+  manager.completeTask(id);
+  showTasksInLists();
+};
+
+function deleteTask(id: number) {
+  // confirm "Are you sure?"
+  if (confirm("Are you sure?")) {
+    manager.deleteTask(id);
+    showTasksInLists();
+  }
+}
+
+function updateDescription(id: number) {
+  // prompt for new description
+  let newDescription = prompt("Enter new description:");
+  if (newDescription != null || newDescription != "") {
+    manager.updateTaskDescription(id, newDescription!);
+    showTasksInLists();
+  } else alert("Sorry! Something went wrong");
+}
+
+console.log(manager.tasks);
+
+//Copyrights
+function copyrights() {
+  const developer = "Oved Harari";
+  const date = new Date();
+  const currentYear = date.getFullYear();
+  console.log(currentYear);
+  document.getElementById("copyrightYear")!.innerText = " " + currentYear;
+  document.getElementById("developer")!.innerText = developer;
+}
+copyrights();
